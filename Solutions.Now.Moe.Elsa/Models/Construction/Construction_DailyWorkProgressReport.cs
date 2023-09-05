4375c454143e628f8c76c204d8b5aa71a8af9a95
mothana-alabsi <alabsi12345667@gmail.com>
2023-09-05 04:55:44 +0300

    edit daily work

---
+++ Solutions.Now.Moe.Elsa/Models/Construction/Construction_DailyWorkProgressReport.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solutions.Now.Moe.Elsa.Models.Construction
+{
+    public class Construction_DailyWorkProgressReport
+    {
+        [Key]
+        public int? serial { get; set; }
+        public int? projectSerial { get; set; }
+        public int? tenderSerial { get; set; }
+    }
+}
