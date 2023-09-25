cdb0b8ae2dc11628db76040d9d9aa6db59618924
almashtooli <user@ALMASHTOOLI>
2023-09-25 11:10:04 +0300

    00

--- Solutions.Now.Moe.Elsa/Models/Construction/InvoicesPayment.cs
+++ Solutions.Now.Moe.Elsa/Models/Construction/InvoicesPayment.cs
@@ -8,5 +8,6 @@ namespace Solutions.Now.Moe.Elsa.Models.Construction
         public int? Serial { get; set; }
         public int? tenderSerial { get; set; }
         public int? status { get; set; }
+        public string accountant { get; set; }
     }
 }
