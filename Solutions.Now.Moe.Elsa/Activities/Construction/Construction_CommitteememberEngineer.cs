2379e15be0f8d21c4f8670f9317c03c55b778d62
DELL <DELL@DESKTOP-HIN9EVR>
2024-02-29 14:36:39 +0300

    Add Construction_CommitteememberEngineer.cs

---
+++ Solutions.Now.Moe.Elsa/Activities/Construction/Construction_CommitteememberEngineer.cs
@@ -0,0 +1,125 @@
+﻿using Elsa.Attributes;
+using Elsa;
+using Solutions.Now.Moe.Elsa.Models;
+using Elsa.Services;
+using Elsa.ActivityResults;
+using Elsa.Expressions;
+using Elsa.Services.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using Solutions.Now.Moe.Elsa.Models.Construction;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Solutions.Now.Moe.Elsa.Common;
+
+namespace Solutions.Now.Moe.Elsa.Activities.Construction
+{
+    [Activity(
+       Category = "Construction",
+       DisplayName = "Communication CommitteememberEngineer Approval",
+       Description = "Communication CommitteememberEngineer Approval in WorkflowRules Table",
+       Outcomes = new[] { OutcomeNames.Done }
+   )]
+    public class Construction_CommitteememberEngineer : Activity
+    {
+        private readonly ConstructionDBContext _ConstructionDBContext;
+        private readonly SsoDBContext _ssoDBContext;
+        private readonly MoeDBContext _moeDBContext;
+
+        public Construction_CommitteememberEngineer(ConstructionDBContext DesignReviewDBContext, SsoDBContext ssoDBContext, MoeDBContext moeDBContext)
+        {
+            _ConstructionDBContext = DesignReviewDBContext;
+            _ssoDBContext = ssoDBContext;
+            _moeDBContext = moeDBContext;
+
+        }
+
+        [ActivityInput(Hint = "Enter an expression that evaluates to the Request Serial.", DefaultSyntax = SyntaxNames.Literal, SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
+        public int? RequestSerial { get; set; }
+        [ActivityInput(Hint = "Enter an expression that evaluates to the workflow type.", DefaultSyntax = SyntaxNames.Literal, SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
+        public int? workflowType { get; set; }
+        [ActivityInput(Hint = "Enter an expression that evaluates to the Section.", DefaultSyntax = SyntaxNames.Literal, SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
+        public int? section { get; set; }
+        [ActivityInput(Hint = "Enter an expression that evaluates to the tender Serial.", DefaultSyntax = SyntaxNames.Literal, SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
+        public int? tenderSerial { get; set; }
+
+        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
+        {
+            List<string> committeemember = new List<string>();
+            HashSet<string> userNameDB = new HashSet<string>();
+            try
+            {
+                if (section == null && tenderSerial == null)
+                {
+                    if (workflowType == WorkFlowsName.Construction_ChangeOrder)
+                    {
+                        var committeeMembers = await _ConstructionDBContext.CommitteeMember.AsAsyncEnumerable().Where(c => c.type == workflowType && c.masterSerial ==null ).ToListAsync();
+                        for (int i = 0; i < committeeMembers.Count; i++)
+                        {
+                            if (!String.IsNullOrEmpty(committeeMembers[i].userName.ToString()))
+                            {
+                                //TblUsers users = await _ssoDBContext.TblUsers.FirstOrDefaultAsync(u => u.username == committeeMember[i].userName);
+                                committeemember.Add(committeeMembers[i].userName);
+
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        var committeeMembers = await _ConstructionDBContext.CommitteeMember.AsAsyncEnumerable().Where(c =>c.type == workflowType).ToListAsync();
+                        for (int i = 0; i < committeeMembers.Count; i++)
+                        {
+                            if (!String.IsNullOrEmpty(committeeMembers[i].userName.ToString()))
+                            {
+                                //TblUsers users = await _ssoDBContext.TblUsers.FirstOrDefaultAsync(u => u.username == committeeMember[i].userName);
+                                committeemember.Add(committeeMembers[i].userName);
+
+                            }
+
+                        }
+                    }
+                }
+                else if (section != null && tenderSerial == null)
+                {
+                    var committeeMembers = await _ConstructionDBContext.CommitteeMember.AsAsyncEnumerable().Where(c =>  c.type == workflowType && c.section == section).ToListAsync();
+
+                    for (int i = 0; i < committeeMembers.Count; i++)
+                    {
+                        if (!String.IsNullOrEmpty(committeeMembers[i].userName.ToString()))
+                        {
+                            committeemember.Add(committeeMembers[i].userName);
+
+                        }
+
+                    }
+                }
+                else if (section == null && tenderSerial != null)
+                {
+                    var sCommittee = await _ConstructionDBContext.SupervisionCommittee.FirstOrDefaultAsync(c => c.serial == tenderSerial);
+
+                    var committeeMembers = await _ConstructionDBContext.CommitteeMember.AsAsyncEnumerable().Where(c => c.tenderSerial == sCommittee.tenderSerial && c.type == workflowType).ToListAsync();
+                    for (int i = 0; i < committeeMembers.Count; i++)
+                    {
+                        if (!String.IsNullOrEmpty(committeeMembers[i].userName.ToString()))
+                        {
+                            //TblUsers users = await _ssoDBContext.TblUsers.FirstOrDefaultAsync(u => u.username == committeeMember[i].userName);
+                            committeemember.Add(committeeMembers[i].userName);
+
+                        }
+
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            context.Output = committeemember;
+            return Done();
+        }
+    }
+}
