59565a7946ff3b0c7e75dbc46e293ede6677f83a
DELL <DELL@DESKTOP-HIN9EVR>
2024-11-15 02:24:46 +0300

    edit AdminstratorFinancial

--- Solutions.Now.Moe.Elsa/Activities/Construction/Construction_AdoptionMandatoryOperatingContractor.cs
+++ Solutions.Now.Moe.Elsa/Activities/Construction/Construction_AdoptionMandatoryOperatingContractor.cs
@@ -75,6 +75,7 @@ namespace Solutions.Now.Moe.Elsa.Activities
                 }
                 //مدير ادارة الشؤون المالية
                 users = await _ssoDBContext.TblUsers.FirstOrDefaultAsync(u => u.Administration == Hierarchy.AdminstratorFinancial && u.position == Positions.AdministrationHead && u.organization == Organization.MOE);
+                    users = await _ssoDBContext.TblUsers.FirstOrDefaultAsync(u => u.Administration == Hierarchy.AdminstratorFinancial && u.position == Positions.AdministrationHead && u.organization == 2);
                 userNameDB[3] = users.username;
                 //مدير مديرية التربية والتعليم
                 users = await _ssoDBContext.TblUsers.FirstOrDefaultAsync(u => u.Administration == tender.tenderSupervisor && u.position == Positions.AdministrationHead && u.organization == Organization.MOE);
